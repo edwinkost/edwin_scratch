35211956908fb44e2f8b89312c8750a005027bde
Edwin SUtanudjaja <sutanudjaja@cehs-hp>
2015-10-28 23:54:03 +0100

    Add scripts for convert volume to thickness.

---
+++ convert_volume_to_thickness/convert_volume_to_height_framework.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import datetime
+import calendar
+import numpy as np
+
+import pcraster as pcr
+from pcraster.framework import DynamicModel
+
+from outputNetcdf import OutputNetcdf
+import virtualOS as vos
+
+class ConvertVolumeToHeightFramework(DynamicModel):
+
+    def __init__(self, input_netcdf,\
+                       output_netcdf,\
+                       modelTime,\
+                       tmpDir = "/dev/shm/"):
+        DynamicModel.__init__(self) 
+
+        self.input_netcdf = input_netcdf
+        self.output_netcdf = output_netcdf 
+        self.tmpDir = tmpDir
+        self.modelTime = modelTime
+
+        # set clone
+        self.clone_map_file = self.input_netcdf['cell_area']
+        pcr.setclone(self.clone_map_file)
+        self.clone = {}
+        self.clone['cellsize'] = pcr.clone().cellSize()
+        self.clone['rows']     = int(pcr.clone().nrRows()) 
+        self.clone['cols']     = int(pcr.clone().nrCols())
+        self.clone['xUL']      = round(pcr.clone().west(), 2)
+        self.clone['yUL']      = round(pcr.clone().north(), 2)
+
+        # cell area (unit: m2)
+        self.cell_area = pcr.readmap(self.input_netcdf['cell_area'])
+
+        # an object for netcdf reporting
+        self.output = OutputNetcdf(self.clone, self.output_netcdf)       
+        
+        # preparing the netcdf file and make variable:
+        self.output.createNetCDF(self.output_netcdf['file_name'],
+                                 self.output_netcdf['gross_variable_name'],
+                                 self.output_netcdf['variable_unit'])
+        self.output.addNewVariable(self.output_netcdf['file_name'],
+                                   self.output_netcdf['netto_variable_name'],
+                                   self.output_netcdf['variable_unit'])
+        
+    def initial(self): 
+        pass
+
+    def dynamic(self):
+        
+        # update model time using the current pcraster timestep value
+        self.modelTime.update(self.currentTimeStep())
+
+        # reading gross and netto values:
+        if self.modelTime.isLastDayOfMonth():
+
+            gross_value = vos.netcdf2PCRobjClone(ncFile  = self.input_netcdf['gross_file_name'],
+                                                 varName = self.input_netcdf['gross_variable_name'],
+                                                 dateInput = str(self.modelTime.fulldate),
+                                                 useDoy = None,
+                                                 cloneMapFileName = self.clone_map_file)
+
+            netto_value = vos.netcdf2PCRobjClone(ncFile  = self.input_netcdf['netto_file_name'],
+                                                 varName = self.input_netcdf['netto_variable_name'],
+                                                 dateInput = str(self.modelTime.fulldate),
+                                                 useDoy = None,
+                                                 cloneMapFileName = self.clone_map_file)
+            
+            # covering with zero and convert the unit to 
+            gross_value = pcr.cover(gross_value, 0.0)/self.cell_area                                     
+            netto_value = pcr.cover(netto_value, 0.0)/self.cell_area                                     
+
+        # reporting
+        if self.modelTime.isLastDayOfMonth():
+
+            # time stamp 
+            timestepPCR = self.modelTime.timeStepPCR
+            timeStamp = datetime.datetime(self.modelTime.year,\
+                                          self.modelTime.month,\
+                                          self.modelTime.day,0)
+            # write gross value to netcdf 
+            self.output.data2NetCDF(self.output_netcdf['file_name'],\
+                                    self.output_netcdf['gross_variable_name'],\
+                                    gross_value,\
+                                    timeStamp)
+            # write netto value to netcdf 
+            self.output.data2NetCDF(self.output_netcdf['file_name'],\
+                                    self.output_netcdf['netto_variable_name'],\
+                                    netto_value,\
+                                    timeStamp)
+
+        # closing the file at the end of
+        if self.modelTime.isLastTimeStep(): self.output.close(self.output_netcdf['file_name'])
+
---
+++ convert_volume_to_thickness/outputNetcdf.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import datetime
+import time
+import re
+import subprocess
+import netCDF4 as nc
+import numpy as np
+import virtualOS as vos
+
+# the following dictionary is needed to avoid open and closing files
+filecache = dict()
+
+class OutputNetcdf():
+    
+    def __init__(self, clone, output_netcdf):
+        		
+        # latitudes and longitudes
+        cellLength = clone['cellsize']
+        deltaLat = cellLength
+        latMax = clone['yUL'] - deltaLat/2.
+        latMin = clone['yUL'] - deltaLat*clone['rows'] + deltaLat/2.
+        deltaLon = cellLength
+        lonMin = clone['xUL'] + deltaLon/2.
+        lonMax = clone['xUL'] + deltaLon*clone['cols'] - deltaLon/2.
+        
+        # cell centre coordinates
+        self.longitudes = np.arange(clone['xUL'] + clone['cellsize']*0.5, \
+                                    clone['xUL'] + clone['cellsize']*clone['cols'], \
+                                    clone['cellsize'])
+        self.latitudes  = np.arange(clone['yUL'] - clone['cellsize']*0.5, \
+                                    clone['yUL'] - clone['cellsize']*clone['rows'], \
+                                   -clone['cellsize'])
+
+        # netcdf format and zlib setup
+        self.format = output_netcdf['format']
+        self.zlib   = output_netcdf['zlib'] 
+        
+        # netcdf attributes
+        self.attributeDictionary = {}
+        self.attributeDictionary['institution'] = output_netcdf['netcdf_attribute']['institution']
+        self.attributeDictionary['title'      ] = output_netcdf['netcdf_attribute']['title'      ]
+        self.attributeDictionary['source'     ] = output_netcdf['netcdf_attribute']['source'     ]
+        self.attributeDictionary['history'    ] = output_netcdf['netcdf_attribute']['history'    ]
+        self.attributeDictionary['references' ] = output_netcdf['netcdf_attribute']['references' ]
+        self.attributeDictionary['description'] = output_netcdf['netcdf_attribute']['description']
+        self.attributeDictionary['comment'    ] = output_netcdf['netcdf_attribute']['comment'    ]
+        
+    def createNetCDF(self, ncFileName, varName, varUnits, longName=None):
+
+        rootgrp = nc.Dataset(ncFileName,'w',format= self.format)
+
+        #-create dimensions - time is unlimited, others are fixed
+        rootgrp.createDimension('time',None)
+        rootgrp.createDimension('lat',len(self.latitudes))
+        rootgrp.createDimension('lon',len(self.longitudes))
+
+        date_time = rootgrp.createVariable('time','f4',('time',))
+        date_time.standard_name = 'time'
+        date_time.long_name = 'Days since 1901-01-01'
+
+        date_time.units = 'Days since 1901-01-01' 
+        date_time.calendar = 'standard'
+
+        lat= rootgrp.createVariable('lat','f4',('lat',))
+        lat.long_name = 'latitude'
+        lat.units = 'degrees_north'
+        lat.standard_name = 'latitude'
+
+        lon= rootgrp.createVariable('lon','f4',('lon',))
+        lon.standard_name = 'longitude'
+        lon.long_name = 'longitude'
+        lon.units = 'degrees_east'
+
+        lat[:]= self.latitudes
+        lon[:]= self.longitudes
+
+        shortVarName = varName
+        longVarName  = varName
+        if longName != None: longVarName = longName
+
+        var = rootgrp.createVariable(shortVarName,'f4',('time','lat','lon',),fill_value=vos.MV,zlib=self.zlib)
+        var.standard_name = varName
+        var.long_name = longVarName
+        var.units = varUnits
+
+        attributeDictionary = self.attributeDictionary
+        for k, v in attributeDictionary.items(): setattr(rootgrp,k,v)
+
+        rootgrp.sync()
+        rootgrp.close()
+
+    def changeAtrribute(self, ncFileName, attributeDictionary, closeFile = False):
+
+        if ncFileName in filecache.keys():
+            #~ print "Cached: ", ncFileName
+            rootgrp = filecache[ncFileName]
+        else:
+            #~ print "New: ", ncFileName
+            rootgrp = nc.Dataset(ncFileName,'a')
+            filecache[ncFileName] = rootgrp
+
+        for k, v in attributeDictionary.items(): setattr(rootgrp,k,v)
+
+        rootgrp.sync()
+        if closeFile == True: rootgrp.close()
+
+    def addNewVariable(self, ncFileName, varName, varUnits, longName=None, closeFile = False):
+
+        if ncFileName in filecache.keys():
+            #~ print "Cached: ", ncFileName
+            rootgrp = filecache[ncFileName]
+        else:
+            #~ print "New: ", ncFileName
+            rootgrp = nc.Dataset(ncFileName,'a')
+            filecache[ncFileName] = rootgrp
+
+        shortVarName = varName
+
+        var = rootgrp.createVariable(shortVarName,'f4',('time','lat','lon',) ,fill_value=vos.MV,zlib=self.zlib)
+        var.standard_name = varName
+        var.long_name = varName
+        var.units = varUnits
+
+        rootgrp.sync()
+        if closeFile == True: rootgrp.close()
+
+    def data2NetCDF(self, ncFileName, shortVarName, varField, timeStamp, posCnt = None, closeFile = False):
+
+        if ncFileName in filecache.keys():
+            #~ print "Cached: ", ncFileName
+            rootgrp = filecache[ncFileName]
+        else:
+            #~ print "New: ", ncFileName
+            rootgrp = nc.Dataset(ncFileName,'a')
+            filecache[ncFileName] = rootgrp
+
+        date_time = rootgrp.variables['time']
+        if posCnt == None: posCnt = len(date_time)
+        date_time[posCnt] = nc.date2num(timeStamp,date_time.units,date_time.calendar)
+
+        rootgrp.variables[shortVarName][posCnt,:,:] = varField
+
+        rootgrp.sync()
+        if closeFile == True: rootgrp.close()
+
+    def dataList2NetCDF(self, ncFileName, shortVarNameList, varFieldList, timeStamp, posCnt = None, closeFile = False):
+
+        if ncFileName in filecache.keys():
+            #~ print "Cached: ", ncFileName
+            rootgrp = filecache[ncFileName]
+        else:
+            #~ print "New: ", ncFileName
+            rootgrp = nc.Dataset(ncFileName,'a')
+            filecache[ncFileName] = rootgrp
+
+        date_time = rootgrp.variables['time']
+        if posCnt == None: posCnt = len(date_time)
+
+        for shortVarName in shortVarNameList:
+            date_time[posCnt] = nc.date2num(timeStamp,date_time.units,date_time.calendar)
+            rootgrp.variables[shortVarName][posCnt,:,:] = varFieldList[shortVarName]
+
+        rootgrp.sync()
+        if closeFile == True: rootgrp.close()
+
+    def close(self, ncFileName):
+
+        if ncFileName in filecache.keys():
+            #~ print "Cached: ", ncFileName
+            rootgrp = filecache[ncFileName]
+        else:
+            #~ print "New: ", ncFileName
+            rootgrp = nc.Dataset(ncFileName,'a')
+            filecache[ncFileName] = rootgrp
+
+        # closing the file 
+        rootgrp.close()
+
+        # remove ncFilename from filecache
+        if ncFileName in filecache.keys(): filecache.pop(ncFileName, None)
