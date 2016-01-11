2206a04ecdd269b75fc74da11b06bad1c4fbe894
Edwin SUtanudjaja <sutanudjaja@cehs-hp>
2016-01-11 01:37:14 +0100

    Add a R script to summarize the results.

--- calculate_country_water_usage/annual_analysis.R
+++ calculate_country_water_usage/annual_analysis.R
@@ -23,6 +23,7 @@ variable_names = names(first_year_table)[4:length(first_year_table)]
 shp_dbf_table = read.dbf("/projects/0/dfguu/users/edwin/data/country_shp_from_tianyi/World_Polys_High.dbf")
 FID = as.character(seq(0, length(shp_dbf_table$WB_NAMES) - 1, 1))
 shp_dbf_table = cbind(FID, shp_dbf_table)
+shp_dbf_table = shp_dbf_table[,!names(shp_dbf_table)=="WB_RULES"]
 
 # arranging tables 
 for (i_variable in 1:length(variable_names)){
