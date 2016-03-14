ecdf59f2894a28defef8c1ff8ad6d37ffa72b2b1
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-14 20:09:33 +0100

    Add a tool to estimate confining layer thickness.

--- calculate_confining_layer_thickness/estimate_confining_layer_thickness.py
+++ calculate_confining_layer_thickness/estimate_confining_layer_thickness.py
@@ -20,6 +20,9 @@ confining_layer_thickness = pcr.ifthen(confining_layer_extent, pcr.min(500.0, aq
 # extrapolate
 confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.05))
 confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.05))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.05))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.05))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.05))
 confining_layer_thickness = pcr.cover(confining_layer_thickness, 0.0)
 confining_layer_thickness_output_filename = "/home/edwin/data/inge_aquifer_parameters/confining_layer_thickness.map"
 pcr.report(confining_layer_thickness, confining_layer_thickness_output_filename)
