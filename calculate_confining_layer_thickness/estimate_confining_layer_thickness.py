dc571e01bfff1183dc2adb141f3b1a71b1830321
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-14 19:33:56 +0100

    Add a tool to estimate confining layer thickness.

---
+++ calculate_confining_layer_thickness/estimate_confining_layer_thickness.py
@@ -0,0 +1,30 @@
+
+import pcraster as pcr
+import netCDF4 as nc
+
+# clone map
+clone_map_file_name = "/projects/0/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+pcr.setclone(clone_map_file_name)
+
+# aquifer thickness map:
+aquifer_thickness_file_name = "/projects/0/dfguu/users/edwin/data/aquifer_properties/thickness_05min.map"
+aquifer_thickness = pcr.readmap(aquifer_thickness_file_name)
+
+# extent of confining layer
+extent_of_confining_layer_file_name = "/home/edwin/data/inge_aquifer_parameters/conflayers4.map"
+confining_layer_extent = pcr.boolean(pcr.readmap(extent_of_confining_layer_file_name))
+
+# thickness of confining layer = 10 percent from the first 500 m
+confining_layer_thickness = pcr.ifthen(confining_layer_extent, pcr.min(500.0, aquifer_thickness)) * 0.10
+
+# extrapolate with window size 0.5 arc degree
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(confining_layer_thickness, 0.5))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, 0.0)
+confining_layer_thickness_output_filename = "/home/edwin/data/inge_aquifer_parameters/confining_layer_thickness.map"
+pcr.report(confining_layer_thickness, confining_layer_thickness_output_filename)
+
+# masking only to the landmask 
+landmask_file_name = "/projects/0/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+landmask = pcr.defined(landmask_file_name)
+confining_layer_thickness_masked_output_filename = "/home/edwin/data/inge_aquifer_parameters/confining_layer_thickness.masked.map"
+pcr.report(pcr.ifthen(landmask, confining_layer_thickness), confining_layer_thickness_masked_output_filename)
