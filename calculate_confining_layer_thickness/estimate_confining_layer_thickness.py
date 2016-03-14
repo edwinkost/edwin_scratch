f04e511d33932ce557c1692f11053de6244c562e
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-14 20:11:48 +0100

    Add a tool to estimate confining layer thickness.

--- calculate_confining_layer_thickness/estimate_confining_layer_thickness.py
+++ calculate_confining_layer_thickness/estimate_confining_layer_thickness.py
@@ -18,11 +18,11 @@ confining_layer_extent = pcr.boolean(pcr.readmap(extent_of_confining_layer_file_
 confining_layer_thickness = pcr.ifthen(confining_layer_extent, pcr.min(500.0, aquifer_thickness)) * 0.05
 
 # extrapolate
-confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.15))
-confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.15))
-confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.15))
-confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.15))
-confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.15))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.25))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.25))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.25))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.25))
+confining_layer_thickness = pcr.cover(confining_layer_thickness, pcr.windowaverage(pcr.cover(confining_layer_thickness, 0.0), 0.25))
 confining_layer_thickness = pcr.cover(confining_layer_thickness, 0.0)
 confining_layer_thickness_output_filename = "/home/edwin/data/inge_aquifer_parameters/confining_layer_thickness.map"
 pcr.report(confining_layer_thickness, confining_layer_thickness_output_filename)
