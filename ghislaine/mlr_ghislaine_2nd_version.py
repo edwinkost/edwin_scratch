2b91fa5b87407f22fa9e780a673b96b561a1bb0b
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-01-22 18:09:32 +0100

    include p-values for mlr

---
+++ ghislaine/mlr_ghislaine_2nd_version.py
@@ -0,0 +1,211 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import LeavePOut
+
+from sklearn.model_selection import cross_val_score
+
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+
+
+# calculate performance values
+def calculate_performance(predictors, target_input, model_input):
+
+    predictions = model_input.predict(predictors)
+    
+    # - r squared and adj_r_squared
+    if len(target_input) > 1:
+        r_squared     = np.corrcoef(target_input, predictions)[0,1]**2.0
+        adj_r_squared = 1 - (1-r_squared)*(len(target_input)-1)/(len(target_input)-predictors.shape[1]-1)
+    else:
+        r_squared     = -9999
+        adj_r_squared = -9999
+    
+    # - rmse and mae
+    if len(target_input) > 1:
+        rmse        = (mean_squared_error(target_input, predictions))**0.5
+        mae         = mean_absolute_error(target_input, predictions)
+    else:
+        rmse = ((predictions - target_input)**2.0)**0.5
+        mae  = np.absolute(predictions - target_input)
+    
+    
+    # ~ print(rmse)
+    # ~ print(mae)
+    
+    # ~ print(predictions, target_input)
+    
+    return r_squared, adj_r_squared, rmse, mae 
+
+
+
+
+# read dataset
+dataset = pd.read_csv("example_uk.csv", encoding='ISO-8859-1', delimiter=',')
+print(dataset.to_string())
+# - replace 1.00E+31 with NaN
+dataset.replace(1.00E+31, np.nan, inplace=True)
+# - drop all rows with NaN
+dataset = dataset.dropna()
+# - reset index
+dataset = dataset.reset_index(drop = True)
+
+# define the target variable
+#~ target = dataset["Species normalized"].astype(float)
+target = dataset["Species"].astype(float)
+
+
+
+
+
+# define the predictors
+# - starting with an empty dataframe
+predictors = pd.DataFrame()
+# - using the following variables Area, Groundwater recharge,Groundwater depth,Evaporation,Discharge,Salinity,BOD,TP,NOXN,bod,ec
+predictors["Area"]                 = dataset["Area"]
+predictors["Groundwater recharge"] = dataset["Groundwater recharge"]
+predictors["Groundwater depth"]    = dataset["Groundwater depth"]
+predictors["Evaporation"]          = dataset["Evaporation"]
+predictors["Discharge"]            = dataset["Discharge"]
+predictors["Salinity"]             = dataset["Salinity"]
+predictors["BOD"]                  = dataset["BOD"]
+predictors["TP"]                   = dataset["TP"]
+predictors["NOXN"]                 = dataset["NOXN"]
+predictors["bod"]                  = dataset["bod"]
+predictors["ec"]                   = dataset["ec"]
+
+
+# fit the model using all data - using sklearn
+mlr_model = LinearRegression()
+mlr_model.fit(predictors,
+ target)
+
+# fit the model using all data - using ols
+reg_ols = sm.OLS(targett, sm.add_constant(predictors)).fit()
+print(reg_ols.summary())
+
+# intercept and regression coefficients
+print("intercept and regression coefficients (using all data)")
+print(mlr_model.intercept_)
+print(mlr_model.coef_)
+
+# get the prediction values (fitted values)
+predictions = mlr_model.predict(predictors)
+
+# calculate also the prediction errors
+errors = predictions - target
+
+# store the prediction and error in a csv file
+predictions_and_errors = pd.DataFrame(\
+                             {
+                              'measurement'    : pd.Series(dtype='float'),
+                              'prediction'     : pd.Series(dtype='float'),
+                              'error'          : pd.Series(dtype='float')
+                              })
+for i_row in range(len(target)): 
+    predictions_and_errors.loc[i_row] = {
+                              'measurement'    : target[i_row],
+                              'prediction'     : predictions[i_row],
+                              'error'          : errors[i_row]
+                              }
+# write data frame to a csv file
+predictions_and_errors.to_csv("predictions_and_erros.csv", index = False)  
+
+# get performance values
+r_squared_all, adj_r_squared_all, rmse_all, mae_all = calculate_performance(predictors, target, mlr_model) 
+print("r_squared_all, adj_r_squared_all, rmse_all, mae_all")   
+print(r_squared_all, adj_r_squared_all, rmse_all, mae_all)   
+print("")
+
+
+# empty table/data frame for storing the the result from the model 
+result_df_empty = pd.DataFrame(\
+                             {
+                              'i'                   : pd.Series(dtype='int'),
+                              'intercept'           : pd.Series(dtype='float'),
+                              'reg_coef_1'          : pd.Series(dtype='float'),
+                              'reg_coef_2'          : pd.Series(dtype='float'),
+                              'reg_coef_3'          : pd.Series(dtype='float'),
+                              'reg_coef_4'          : pd.Series(dtype='float'),
+                              'reg_coef_5'          : pd.Series(dtype='float'),
+                              'reg_coef_6'          : pd.Series(dtype='float'),
+                              'reg_coef_7'          : pd.Series(dtype='float'),
+                              'reg_coef_8'          : pd.Series(dtype='float'),
+                              'reg_coef_9'          : pd.Series(dtype='float'),
+                              'reg_coef_10'         : pd.Series(dtype='float'),
+                              'reg_coef_11'         : pd.Series(dtype='float'),
+                              'r_squared_train'     : pd.Series(dtype='float'),
+                              'adj_r_squared_train' : pd.Series(dtype='float'),
+                              'rmse_train'          : pd.Series(dtype='float'),
+                              'mae_train'           : pd.Series(dtype='float'),
+                              'r_squared_test'      : pd.Series(dtype='float'),
+                              'adj_r_squared_test'  : pd.Series(dtype='float'),
+                              'rmse_test'           : pd.Series(dtype='float'),
+                              'mae_test'            : pd.Series(dtype='float'),
+                             })
+
+# result from the model fitted using all data
+result_df_all = result_df_empty
+new_row = None
+del new_row
+new_row = {
+            'i'                   : -1,
+            'intercept'           : mlr_model.intercept_,
+            'reg_coef_1'          : mlr_model.coef_[0],
+            'reg_coef_2'          : mlr_model.coef_[1],
+            'reg_coef_3'          : mlr_model.coef_[2],
+            'reg_coef_4'          : mlr_model.coef_[3],
+            'reg_coef_5'          : mlr_model.coef_[4],
+            'reg_coef_6'          : mlr_model.coef_[5],
+            'reg_coef_7'          : mlr_model.coef_[6],
+            'reg_coef_8'          : mlr_model.coef_[7],
+            'reg_coef_9'          : mlr_model.coef_[8],
+            'reg_coef_10'         : mlr_model.coef_[9],
+            'reg_coef_11'         : mlr_model.coef_[10],
+            'r_squared_train'     : r_squared_all,
+            'adj_r_squared_train' : adj_r_squared_all,
+            'rmse_train'          : rmse_all,
+            'mae_train'           : mae_all,
+            'r_squared_test'      : -9999,
+            'adj_r_squared_test'  : -9999,
+            'rmse_test'           : -9999,
+            'mae_test'            : -9999
+           }
+print(new_row)
+result_df_all.loc[len(result_df_all)] = new_row
+# write data frame to a csv file
+result_df_all.to_csv("cv_result_all_example_uk.csv"  , index = False)  
+
+                         
+                             
+# get the predicted value
+predictions = pd.DataFrame(mlr_model.predict(predictors))
+print(predictions)
+
+
+# create scatter plots between 'measurements (y)' and 'predicted values (x)'
+plt.figure(figsize=(8, 6))
+plt.scatter(x = target, y = predictions)
+plt.title('Scatter Plot: Predictions vs Measurements')
+
+# add the trendline to the plot
+model_linear_plot = LinearRegression()
+
+print(predictions)
+print(target)
+
+model_linear_plot.fit(predictions, target)
+plt.plot(predictions, model_linear_plot.predict(predictions), color='red', linewidth=2)
+plt.savefig('mlr_result.png')
+plt.show()
